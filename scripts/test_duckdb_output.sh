4714046112676beb1c0433faa5b6c6e2e0d8024a
Christiaan Herrewijn <christiaan@duckdblabs.com>
2024-09-23 17:31:04 +0200

    improve script to analyse results of duckdb storage file fuzzer

--- scripts/test_duckdb_output.sh
+++ scripts/test_duckdb_output.sh
@@ -4,18 +4,24 @@
 
 COUNTER=0
 ERROR_COUNTER=0
+CRASH_COUNTER=0
 
 mkdir -p tmp
-for fuzz_result in ../fuzz_results/duckdb_file_fuzzer/default/queue/*;
+for fuzz_result in ../fuzz_results/duckdb_file_fuzzer/default/crashes/*;
 do
     cp $fuzz_result ./tmp/file_$COUNTER
     python3 ./fix_filesize_header_checksums.py ./tmp/file_$COUNTER
     duckdb -c "ATTACH './tmp/file_$COUNTER' AS tmp_db (READ_ONLY); use tmp_db; show tables;" > /dev/null
-    if [ "$?" -eq "1" ]; then
-        echo "error in file_$COUNTER"
-        ((ERROR_COUNTER+=1))
+    exit_status=$?
+    if [ "$exit_status" -ne "0" ]; then
+        echo "error in file_$COUNTER, exit status: $exit_status"
+        if [ "$exit_status" -eq "1" ]; then
+            ((ERROR_COUNTER+=1))
+        else
+            ((CRASH_COUNTER+=1))
+        fi
     fi
     ((COUNTER+=1))
 done
-echo "$COUNTER duckdb files have been read, nr of (graceful) errors: '$ERROR_COUNTER'"
+echo "$COUNTER duckdb files have been read, nr of graceful errors: '$ERROR_COUNTER', nr of crashes: $CRASH_COUNTER"
 rm -rf ./tmp/
