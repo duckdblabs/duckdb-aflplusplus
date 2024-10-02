62dacc4df56700e30a17a5887d867381dfc4cacf
Christiaan Herrewijn <christiaan@duckdblabs.com>
2024-10-02 11:04:53 +0200

    reduce hardcoded paths

--- scripts/create_wal_file_corpus.sh
+++ scripts/create_wal_file_corpus.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [[ -z "${CORPUS_DIR}" ]]; then
-  wal_corpus_dir="./corpus/walfiles"
+  wal_corpus_dir=$(dirname $(dirname $(realpath $0)))/corpus/walfiles
 else
   wal_corpus_dir="${CORPUS_DIR}/walfiles"
 fi
 
 if [[ -z "${BUILD_DIR}" ]]; then
-  build_dir="./build"
+  build_dir=$(dirname $(dirname $(realpath $0)))/build
 else
-  build_dir="${BUILD_DIR}/walfiles"
+  build_dir="${BUILD_DIR}"
 fi
 
 # delete existing corpus
@@ -33,15 +33,15 @@ CHECKPOINT;
 duckdb base_db -c "$q_init" > /dev/null
 
 # create wal files by doing transactions without checkpointing
-for mod_file in ./scripts/wal_file_corpus_init/*.sql;
+for mod_file in $(dirname $0)/wal_file_corpus_init/*.sql
 do
-    cp base_db tmp_db
-    query="PRAGMA disable_checkpoint_on_shutdown;"$(cat $mod_file)
-    duckdb "tmp_db" -c "$query" > /dev/null
-    mv tmp_db.wal ${wal_corpus_dir}/$(basename $mod_file '.sql').wal
+  cp base_db tmp_db
+  query="PRAGMA disable_checkpoint_on_shutdown;"$(cat $mod_file)
+  duckdb "tmp_db" -c "$query" > /dev/null
+  mv tmp_db.wal ${wal_corpus_dir}/$(basename $mod_file '.sql').wal
 done
 rm -f tmp_db
 
 # base_db is considered a build file
-mkdir -p build
+mkdir -p $build_dir
 mv base_db ${build_dir}/base_db
