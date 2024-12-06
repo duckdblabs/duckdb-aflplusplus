0e6fbe23631f1d888f4da20bee50bf1756ba7ae5
Christiaan Herrewijn <christiaan@duckdblabs.com>
2024-12-06 17:10:22 +0100

    fuzz csv reader with multiple parameters

--- scripts/create_prepended_csv_corpus.py
+++ scripts/create_prepended_csv_corpus.py
@@ -14,65 +14,67 @@ CSV_CORPUS_JSON = CSV_CORPUS_ROOT_DIR / 'csv_parameter.json'
 OUT_DIR = CSV_CORPUS_ROOT_DIR / 'corpus_prepended'
 DUCKDB_DIR = FILE_DIR_TO_SCRAPE = Path('~/git/duckdb/').expanduser()
 
-# note: types here are simplified to 'BOOLEAN', 'INTEGER', and 'VARCHAR'
+# NOTE: keep this list in sync with 'csv_parameter_flex_fuzzer.cpp' !!
+# NOTE: types here are simplified to 'BOOLEAN', 'INTEGER', and 'VARCHAR'
 # VARCHAR is also used for all complex types (lists, structs, etc)
 READ_CSV_PARAMETERS = [
-    {"name": "all_varchar", "type": "BOOLEAN"},
-    {"name": "allow_quoted_nulls", "type": "BOOLEAN"},
-    {"name": "auto_detect", "type": "BOOLEAN"},
-    {"name": "auto_type_candidates", "type": "VARCHAR"},
-    {"name": "columns", "type": "VARCHAR"},
-    {"name": "compression", "type": "VARCHAR"},
-    {"name": "dateformat", "type": "VARCHAR"},
-    {"name": "decimal_separator", "type": "VARCHAR"},
-    {"name": "delim", "type": "VARCHAR"},
-    {"name": "delimiter", "type": "VARCHAR"},
-    {"name": "dtypes", "type": "VARCHAR"},
-    {"name": "escape", "type": "VARCHAR"},
-    {"name": "filename", "type": "BOOLEAN"},
-    {"name": "force_not_null", "type": "VARCHAR"},
-    {"name": "header", "type": "BOOLEAN"},
-    {"name": "hive_partitioning", "type": "BOOLEAN"},
-    {"name": "ignore_errors", "type": "BOOLEAN"},
-    {"name": "max_line_size", "type": "INTEGER"},
-    {"name": "names", "type": "VARCHAR"},
-    {"name": "new_line", "type": "VARCHAR"},
-    {"name": "normalize_names", "type": "BOOLEAN"},
-    {"name": "null_padding", "type": "BOOLEAN"},
-    {"name": "nullstr", "type": "VARCHAR"},
-    {"name": "parallel", "type": "BOOLEAN"},
-    {"name": "quote", "type": "VARCHAR"},
-    {"name": "sample_size", "type": "INTEGER"},
-    {"name": "sep", "type": "VARCHAR"},
-    {"name": "skip", "type": "INTEGER"},
-    {"name": "timestampformat", "type": "VARCHAR"},
-    {"name": "types", "type": "VARCHAR"},
-    {"name": "union_by_name", "type": "BOOLEAN"},
+    ("all_varchar", "BOOLEAN"),
+    ("allow_quoted_nulls", "BOOLEAN"),
+    ("auto_detect", "BOOLEAN"),
+    ("auto_type_candidates", "VARCHAR"),
+    ("columns", "VARCHAR"),
+    ("compression", "VARCHAR"),
+    ("dateformat", "VARCHAR"),
+    ("decimal_separator", "VARCHAR"),
+    ("delim", "VARCHAR"),
+    ("delimiter", "VARCHAR"),
+    ("dtypes", "VARCHAR"),
+    ("escape", "VARCHAR"),
+    ("filename", "BOOLEAN"),
+    ("force_not_null", "VARCHAR"),
+    ("header", "BOOLEAN"),
+    ("hive_partitioning", "BOOLEAN"),
+    ("ignore_errors", "BOOLEAN"),
+    ("max_line_size", "INTEGER"),
+    ("names", "VARCHAR"),
+    ("new_line", "VARCHAR"),
+    ("normalize_names", "BOOLEAN"),
+    ("null_padding", "BOOLEAN"),
+    ("nullstr", "VARCHAR"),
+    ("parallel", "BOOLEAN"),
+    ("quote", "VARCHAR"),
+    ("sample_size", "INTEGER"),
+    ("sep", "VARCHAR"),
+    ("skip", "INTEGER"),
+    ("timestampformat", "VARCHAR"),
+    ("types", "VARCHAR"),
+    ("union_by_name", "BOOLEAN"),
+
     # undocumented
-    {"name": "buffer_size", "type": "INTEGER"},
-    {"name": "column_names", "type": "VARCHAR"},
-    {"name": "column_types", "type": "VARCHAR"},
-    {"name": "comment", "type": "VARCHAR"},
-    {"name": "date_format", "type": "VARCHAR"},
-    {"name": "encoding", "type": "VARCHAR"},
-    {"name": "force_quote", "type": "VARCHAR"},
-    {"name": "hive_type", "type": "VARCHAR"},
-    {"name": "hive_type_autocast", "type": "BOOLEAN"},
-    {"name": "hive_types", "type": "VARCHAR"},
-    {"name": "hive_types_autocast", "type": "BOOLEAN"},
-    {"name": "maximum_line_size", "type": "INTEGER"},
-    {"name": "null", "type": "VARCHAR"},
-    {"name": "prefix", "type": "VARCHAR"},
-    {"name": "rejects_limit", "type": "INTEGER"},
-    {"name": "rejects_scan", "type": "VARCHAR"},
-    {"name": "rejects_table", "type": "VARCHAR"},
-    {"name": "rfc_4180", "type": "BOOLEAN"},
-    {"name": "store_rejects", "type": "BOOLEAN"},
-    {"name": "suffix", "type": "VARCHAR"},
-    {"name": "timestamp_format", "type": "VARCHAR"},
+    ("buffer_size", "INTEGER"),
+    ("column_names", "VARCHAR"),
+    ("column_types", "VARCHAR"),
+    ("comment", "VARCHAR"),
+    ("date_format", "VARCHAR"),
+    ("encoding", "VARCHAR"),
+    ("force_quote", "VARCHAR"),
+    ("hive_type", "VARCHAR"),
+    ("hive_type_autocast", "BOOLEAN"),
+    ("hive_types", "VARCHAR"),
+    ("hive_types_autocast", "BOOLEAN"),
+    ("maximum_line_size", "INTEGER"),
+    ("null", "VARCHAR"),
+    ("prefix", "VARCHAR"),
+    ("rejects_limit", "INTEGER"),
+    ("rejects_scan", "VARCHAR"),
+    ("rejects_table", "VARCHAR"),
+    ("rfc_4180", "BOOLEAN"),
+    ("store_rejects", "BOOLEAN"),
+    ("suffix", "VARCHAR"),
+    ("timestamp_format", "VARCHAR"),
 ]
 
-PARAMETER_NAMES = [param['name'] for param in READ_CSV_PARAMETERS]
+READ_CSV_PARAMETERS_DICT = {param[0]: (param_idx, param[1]) for param_idx, param in enumerate(READ_CSV_PARAMETERS)}
 
 
 def main():
@@ -93,8 +95,8 @@ def main():
             continue
         file_name = f"{count:04d}_prepended_csv"
         csv_content_bytes = (DUCKDB_DIR / Path(scenario['data_file'])).read_bytes()
-        # exclude big files, they are less suitable for fuzzing
-        if len(csv_content_bytes) > 500:
+        # exclude larger files, they are less suitable for fuzzing
+        if len(csv_content_bytes) > 5000:
             nr_rejcts_csv_content_too_long += 1
             continue
         argument_bytes = encode_csv_arguments(scenario['arguments'], count)
@@ -111,7 +113,7 @@ def main():
 def encode_csv_arguments(arguments: dict[str, str], scenario_count: int) -> bytes:
     # header: 1 byte with the number of arguments
     assert len(arguments) < 256
-    encoded_arguments = len(arguments).to_bytes()
+    encoded_arguments = len(arguments).to_bytes(1)
 
     # encoding per argument:
     # 1 byte: param_name (enum)
@@ -119,11 +121,11 @@ def encode_csv_arguments(arguments: dict[str, str], scenario_count: int) -> byte
     # N bytes: argument value
     for param_name, value in sorted(arguments.items()):
         assert len(value) < 256
-        try:
-            param_idx = PARAMETER_NAMES.index(param_name)
-        except ValueError:
+
+        if param_name not in READ_CSV_PARAMETERS_DICT:
             raise ValueError(f'error: unknown argument: {param_name}')
-        param_type = READ_CSV_PARAMETERS[param_idx]['type']
+        param_idx = READ_CSV_PARAMETERS_DICT[param_name][0]
+        param_type = READ_CSV_PARAMETERS_DICT[param_name][1]
         match param_type:
             case 'BOOLEAN':
                 if 'true' in value.lower() or '1' in value:
@@ -134,11 +136,13 @@ def encode_csv_arguments(arguments: dict[str, str], scenario_count: int) -> byte
                     raise ValueError(f"invalid boolean value: {value}")
                 encoded_arguments = encoded_arguments + (param_idx.to_bytes(1) + (1).to_bytes(1) + value.encode())
             case 'INTEGER':
-                if not value.isnumeric():
+                try:
+                    value = int(value)
+                except ValueError:
                     print(f"value '{value}' not usable for param '{param_name}'; default value '42' used instead.")
                     value = 42
                 encoded_arguments = encoded_arguments + (
-                    param_idx.to_bytes(1) + (8).to_bytes(1) + int(value).to_bytes(8)
+                    param_idx.to_bytes(1) + (8).to_bytes(1) + int(value).to_bytes(length=8, byteorder='little', signed=True)
                 )
             case 'VARCHAR':
                 encoded_arguments = encoded_arguments + (
