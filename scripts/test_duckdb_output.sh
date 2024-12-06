dcf1ed1bd03c308ec837252b25d46bed1fcdbc62
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-06 11:04:38 +0100

    we'd like to collect files groupped into 3 cathegories: raw(crashes) and with the fixed checksum causing internal error or crash

--- scripts/test_duckdb_output.sh
+++ scripts/test_duckdb_output.sh
@@ -7,7 +7,8 @@ ERROR_COUNTER=0
 CRASH_COUNTER=0
 
 mkdir -p tmp
-mkdir -p report
+mkdir -p fixed_checksum_internal_error_files
+mkdir -p fixed_checksum_crash_files
 for fuzz_result in ../fuzz_results/duckdb_file_fuzzer/default/crashes/*;
 do
     cp $fuzz_result ./tmp/file_$COUNTER
@@ -17,13 +18,13 @@ do
     if [ "$exit_status" -ne "0" ]; then
         echo "error in file_$COUNTER, exit status: $exit_status"
         if [ "$exit_status" -eq "1" ]; then
-            cp $COUNTER report
             ((ERROR_COUNTER+=1))
+            cp ./tmp/file_$COUNTER fixed_checksum_internal_error_files
         else
             ((CRASH_COUNTER+=1))
+            cp ./tmp/file_$COUNTER fixed_checksum_crash_files
         fi
     fi
     ((COUNTER+=1))
 done
 echo "$COUNTER duckdb files have been read, nr of graceful errors: '$ERROR_COUNTER', nr of crashes: $CRASH_COUNTER"
-rm -rf ./tmp/
