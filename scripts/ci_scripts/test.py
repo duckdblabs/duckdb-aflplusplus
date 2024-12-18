52440a82105c001088edaa34efef6a683782874f
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-18 16:50:34 +0100

    scripts/ci_scripts/test.py

--- scripts/ci_scripts/test.py
+++ scripts/ci_scripts/test.py
@@ -10,9 +10,39 @@ except subprocess.CalledProcessError as e:
 
 
 file_types = ["csv", "json", "parquet", "duckdb", "wal"]
+# fuzzing_type = ["file", "pipe", "file-parameter"]
 for file_type in file_types:
+    fuzz_command = [
+        "make",
+        f"fuzz-{ file_type }-file"
+        ]
     try:
-        subprocess.run(["make", f"fuzz-{ file_type }-file"], check=True)
+        subprocess.run(fuzz_command, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Make fuzz command failed with error: {e}")
 
+    # if file_type in ["csv", "json", "parquet"]:
+    if file_type in ["csv", "parquet"]:
+        for file_type in ["csv", "parquet"]:
+            reproduce_command = [
+                "bash",
+                f"scripts/test_{ file_type }_reader.sh"
+                ]
+    elif file_type == 'duckdb':
+        reproduce_command = [
+            "bash",
+            "scripts/test_duckdb_output.sh"
+        ]
+    else: # wal does everything for all crash files
+        fix_command = [
+            "bash",
+            "./scripts/fix_wal_files.sh"
+        ]
+        reproduce_command = [
+            "bash",
+            "./scripts/wal_replay.sh"
+        ]
+        try:
+            subprocess.run(fix_command, check=True, text=True, capture_output=True)
+    try:
+        subprocess.run(reproduce_command, check=True, text=True, capture_output=True)
