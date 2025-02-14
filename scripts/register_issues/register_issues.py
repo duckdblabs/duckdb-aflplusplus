fb8903324c97a1dc76ee9557449e9f73a522fe6f
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-02-14 13:08:51 +0100

    attempt to use FUZZEROFDUCKSKEY do download artifacts

--- scripts/register_issues/register_issues.py
+++ scripts/register_issues/register_issues.py
@@ -7,5 +7,10 @@ def main():
     body = 'This is a dummy issue to test if the permissions work correctly to create issues from duckdb_aflplusplus.\nFeel free to close this issue.'
     # github_helper.make_github_issue(title, body)
 
+    # debug validate token
+    print("validate token..")
+    github_helper.get_token()
+    print("ok..")
+
 if __name__ == "__main__":
     main()
