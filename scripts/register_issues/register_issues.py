7d2f2defac340d7d37fa450df2a1701937abf2ae
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-02-14 12:44:46 +0100

    add extract reproductions

--- scripts/register_issues/register_issues.py
+++ scripts/register_issues/register_issues.py
@@ -5,7 +5,7 @@ import github_helper
 def main():
     title = 'dummy AFL++ issue'
     body = 'This is a dummy issue to test if the permissions work correctly to create issues from duckdb_aflplusplus.\nFeel free to close this issue.'
-    github_helper.make_github_issue(title, body)
+    # github_helper.make_github_issue(title, body)
 
 if __name__ == "__main__":
     main()
