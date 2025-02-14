23ad358af8d2d5c377c98932fcb7da1ad455b19c
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-02-14 14:08:10 +0100

    verify all info is available to register_issues script

--- scripts/register_issues/register_issues.py
+++ scripts/register_issues/register_issues.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import github_helper
+from pathlib import Path
+import os
 
 def main():
     title = 'dummy AFL++ issue'
@@ -11,6 +13,14 @@ def main():
     print("validate token..")
     github_helper.get_token()
     print("ok..")
+
+    crashes_dir = Path('./reproductions/crashes')
+    hangs_dir = Path('./reproductions/hangs')
+    sqllogic_test_crahses = Path('13288614425-csv_multi_param_fuzzer.test')
+    sqllogic_test_hangs = Path('13288614425-csv_multi_param_fuzzer.test')
+
+    print(f"fuzz run id {os.environ['FUZZ_RUN_ID']}")
+    print(sqllogic_test_crahses.read_text())
 
 if __name__ == "__main__":
     main()
