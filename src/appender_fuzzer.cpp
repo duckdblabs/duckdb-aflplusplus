545b2baadf214d8b47980fc7a4ac607a7dc706ab
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-08-11 16:30:50 +0200

    bug fix

--- src/appender_fuzzer.cpp
+++ src/appender_fuzzer.cpp
@@ -5,13 +5,14 @@
 #include <unistd.h>
 
 const int NR_SCENARIO_BYTES = 3;
-const int BUFF_SIZE = 4096;
+const int BUFF_SIZE = 10;
 
 void AppenderFuzzer() {
+	assert(BUFF_SIZE > NR_SCENARIO_BYTES);
+
 	duckdb::DuckDB db(nullptr);
 	duckdb::Connection con(db);
-
-	uint8_t buf[4096];
+	uint8_t buf[BUFF_SIZE];
 	uint8_t scenario_buf[NR_SCENARIO_BYTES];
 
 	enum read_state_option {
@@ -26,63 +27,61 @@ void AppenderFuzzer() {
 	int idx_start_char;
 	std::string val1;
 	std::string val2;
-	read_state_option read_stat = SCENARIO_BYTES;
+	read_state_option read_state = SCENARIO_BYTES;
 
 	// append records (read from stdin)
 	con.Query("PRAGMA force_compression='fsst'");
 	con.Query("CREATE OR REPLACE TABLE tbl (col1 VARCHAR, col2 VARCHAR)");
 	duckdb::Appender appender(con, "tbl");
 	while (n_read != 0) {
+		// before reading new chunk, preserve partial values of previous chunck
+		if (idx_buf >= idx_start_char) {
+			if (read_state == FIRST_VAL) {
+				val1 = val1 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf - idx_start_char);
+			}
+			if (read_state == SECOND_VAL) {
+				val2 = val2 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf - idx_start_char);
+			}
+		}
+		// read new chunck
 		n_read = read(0, (void *)buf, BUFF_SIZE);
 		if (n_read < 0) {
 			std::cout << "error in read: " << n_read << std::endl;
 			perror(NULL);
 			exit(EXIT_FAILURE);
 		}
-
-		// split rows by newline, split values by comma (if any)
+		// parse chunck: split rows by newline, split values by first comma (if any)
 		idx_buf = 0;
-
+		idx_start_char = 0;
 		while (idx_buf < n_read) {
-			if (read_stat == SCENARIO_BYTES) {
+			if (read_state == SCENARIO_BYTES) {
 				assert(idx_scenario_byte < NR_SCENARIO_BYTES);
 				scenario_buf[idx_scenario_byte] = buf[idx_buf];
 				idx_scenario_byte++;
 				if (idx_scenario_byte == NR_SCENARIO_BYTES) {
-					read_stat = FIRST_VAL;
+					// reset
+					idx_scenario_byte = 0;
+					read_state = FIRST_VAL;
 					idx_start_char = idx_buf + 1;
 				}
-			}
-			if (buf[idx_buf] == ',' && (read_stat == FIRST_VAL)) {
-				val1 = val1 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf - idx_start_char);
-				idx_start_char = idx_buf + 1;
-				read_stat = SECOND_VAL;
-			}
-			if ((buf[idx_buf] == '\n') && (read_stat != SCENARIO_BYTES)) {
-				if (read_stat == FIRST_VAL) {
+			} else if (buf[idx_buf] == '\n') {
+				if (read_state == FIRST_VAL) {
 					val1 = val1 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf - idx_start_char);
 				}
-				else if (read_stat == SECOND_VAL) {
+				if (read_state == SECOND_VAL) {
 					val2 = val2 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf - idx_start_char);
 				}
-				// add row
-				// TODO: apply scenario bytes to append a preprocessed row
-
-				appender.AppendRow(val1.c_str(), val2.c_str());
+				AppendScenarioRow(appender, scenario_buf, val1, val2);
 				// reset
 				val1 = "";
 				val2 = "";
-				read_stat = SCENARIO_BYTES;
-				idx_scenario_byte = 0;
+				read_state = SCENARIO_BYTES;
 				idx_start_char = idx_buf + 1;
-			}
-			// end of read-buffer, preserve what we have so far
-			if (idx_buf == n_read - 1) {
-				if (read_stat == FIRST_VAL) {
-					val1 = val1 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf + 1 - idx_start_char);
-				} else if (read_stat == SECOND_VAL){
-					val2 = val2 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf + 1 - idx_start_char);
-				}
+			} else if (buf[idx_buf] == ',' && read_state == FIRST_VAL) {
+				assert(idx_buf >= idx_start_char);
+				val1 = val1 + std::string(reinterpret_cast<const char*>(buf + idx_start_char), idx_buf - idx_start_char);
+				idx_start_char = idx_buf + 1;
+				read_state = SECOND_VAL;
 			}
 			idx_buf++;
 		}
@@ -95,10 +94,16 @@ void AppenderFuzzer() {
 	appender.Close();
 
 	// debug
-	// duckdb::unique_ptr<duckdb::QueryResult> result;
-	// result = con.Query("FROM tbl");
-	// std::cout << result->ToString() << std::endl;
-	// std::cout << con.Query("SELECT count(*) FROM tbl")->ToString() << std::endl;
+	duckdb::unique_ptr<duckdb::QueryResult> result;
+	result = con.Query("FROM tbl");
+	std::cout << result->ToString() << std::endl;
+	std::cout << con.Query("SELECT count(*) FROM tbl")->ToString() << std::endl;
+}
+
+void AppendScenarioRow(duckdb::Appender &appender, uint8_t scenario_buf[NR_SCENARIO_BYTES], std::string &val1,
+                       std::string &val2) {
+	// TODO: apply scenario bytes to append a preprocessed row
+	appender.AppendRow(val1.c_str(), val2.c_str());
 }
 
 int main() {
