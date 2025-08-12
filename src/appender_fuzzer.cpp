4c866cd76463c4f74261108e37403a2c31616b6f
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-08-12 14:27:16 +0200

    apply scenario bytes

--- src/appender_fuzzer.cpp
+++ src/appender_fuzzer.cpp
@@ -8,10 +8,99 @@ const int NR_SCENARIO_BYTES = 3;
 const int BUFF_SIZE = 4096;
 
 
+std::string CreatePrintableString(const std::string &str) {
+    std::string printable_str;
+    printable_str.reserve(str.size());
+    for (unsigned char ch : str) {
+        if (std::isprint(ch)) {
+            printable_str.push_back(ch);
+        } else {
+            // convert non-printable chars into ASCII range 32..126 (95 characters)
+            unsigned char printable = (ch % 95) + 32;
+            printable_str.push_back(printable);
+        }
+    }
+    return printable_str;
+}
+
+void RepeatString(std::string& str, size_t n) {
+    assert(n > 0);
+    const size_t original_size = str.size();
+    assert(original_size > 0);
+
+    str.resize(original_size * n);
+    for (size_t i = 1; i < n; ++i) {
+        std::copy(str.begin(), str.begin() + original_size,
+                  str.begin() + i * original_size);
+    }
+}
+
+void GetBoolSettings(uint8_t &scenario_byte, bool (&bool_settings)[8]) {
+    for (int i = 0; i < 8; i++) {
+        bool_settings[7 - i] = (scenario_byte >> i) & 0b00000001;
+    }
+}
+
 void AppendScenarioRow(duckdb::Appender &appender, uint8_t scenario_buf[NR_SCENARIO_BYTES], std::string &val1,
 					   std::string &val2) {
-	// TODO: apply scenario bytes to append a preprocessed row
-	appender.AppendRow(val1.c_str(), val2.c_str());
+	// use first scenario byte to get bool-settings
+	bool bool_settings[8];
+	GetBoolSettings(scenario_buf[0], bool_settings);
+
+	// use second and third scenario byte to get n value [0 - 5000]
+	assert(NR_SCENARIO_BYTES == 3);
+	uint16_t n_value = (static_cast<uint16_t>(scenario_buf[1]) << 8) | static_cast<uint16_t>(scenario_buf[2]) % 5000;
+
+	// apply bool-settings
+	// 0 - replace val1 by ""
+	std::string append_str1 = (bool_settings[0]) ? "" : val1;
+
+	// 1 - replace val2 by ""
+	std::string append_str2 = (bool_settings[1]) ? "" : val2;
+
+	// 2 - replace val1: convert to printable chars
+	if (bool_settings[2] && append_str1 != "") {
+		append_str1 = CreatePrintableString(append_str1);
+	}
+
+	// 3 - replace val2 by val1
+	if (bool_settings[3]) {
+		append_str2 = append_str1;
+	}
+
+	// 4 - repeat val1 n times
+	if (bool_settings[4] && append_str1 != "" && n_value >= 2) {
+		RepeatString(append_str1, n_value);
+	}
+
+	// 5 - repeat val2 n times
+	if (bool_settings[5] && append_str2 != "" && n_value >= 2) {
+		RepeatString(append_str2, n_value);
+	}
+
+	// 6 - append same row n times
+	uint16_t nr_row_inserts = (bool_settings[6]) ? n_value : 1;
+
+	// 7 - replace empty strings by NULL
+	if (bool_settings[7]) {
+		duckdb::Value append_val_1 = (append_str1 == "") ? duckdb::Value(nullptr) : duckdb::Value(append_str1);
+		duckdb::Value append_val_2 = (append_str2 == "") ? duckdb::Value(nullptr) : duckdb::Value(append_str2);
+		for (u_int16_t i = 0; i < nr_row_inserts; i++) {
+			appender.AppendRow(append_val_1, append_val_2);
+		}
+	}
+	else {
+		for (u_int16_t i = 0; i < n_value; i++) {
+			appender.AppendRow(duckdb::string_t(append_str1), duckdb::string_t(append_str2));
+		}
+	}
+
+	// DEBUG: print bool settings:
+	// for (bool b : bool_settings) {
+	// 	std::cout << b << std::endl;
+	// }
+	// std::cout << "-----------" << std::endl;
+	// std::cout << scenario_buf[0] << std::endl;
 }
 
 void AppenderFuzzer() {
