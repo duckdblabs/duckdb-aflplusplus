1b597e07b66fddf0c5162fe57fd1d5cb8dfb9795
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-10-01 11:28:27 +0200

    fixed issue creation after hangs

--- scripts/register_issues/reproduce_and_file_issues.py
+++ scripts/register_issues/reproduce_and_file_issues.py
@@ -12,6 +12,7 @@ import github_helper
 import uuid
 import os
 
+
 def is_internal_error(error):
     if 'differs from original result' in error:
         return True
@@ -40,7 +41,7 @@ def split_exception_trace(exception_msg_full: str) -> tuple[str, str]:
 
     # cleaning:
     # if first line only contains =-symbols, skip it
-    if(re.fullmatch("=*", exception_msg)):
+    if re.fullmatch("=*", exception_msg):
         exception_msg, _, stack_trace = stack_trace.partition('\n')
     # if exception_msg is non-descritive AddressSanitizer issue, use first 2 lines of stack trace instead.
     if "AddressSanitizer: heap-buffer-overflow" in exception_msg and stack_trace:
@@ -58,12 +59,95 @@ def run_command(command):
         exit(res)
 
 
+def reproduce_file_reader_issue(duckdb_cli, repro_file_path, file_reader_function, arguments):
+    sql_statement = f"from {file_reader_function}('{repro_file_path}'{arguments})"
+    (stdout, stderr, returncode, timed_out) = run_duckdb(duckdb_cli, sql_statement)
+    match returncode:
+        case 0:
+            return ("", "")
+        case 1 if not is_internal_error(stderr):  # regular error
+            return ("", "")
+        case 1:  # internal error
+            exception_msg, stacktrace = split_exception_trace(stderr)
+        case _ if returncode < 0:  # crash
+            exception_msg, stacktrace = split_exception_trace(stderr)
+            sig_name = signal.Signals(-returncode).name
+            exception_msg = f"{sig_name}: {exception_msg}"
+        case _ if timed_out: # hang
+            exception_msg, stacktrace = (f"{file_reader_function} timed out after 300 s", "")
+        case _:
+            raise ValueError(f"undefined return code: {returncode} (expected 0, 1, or negative values)")
+    return (exception_msg, stacktrace)
+
+
 def run_duckdb(duckdb_cli, sql_statement):
     command = [duckdb_cli, '-batch', '-init', '/dev/null']
-    res = subprocess.run(command, input=bytearray(sql_statement, 'utf8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout = res.stdout.decode('utf8', 'ignore').strip()
-    stderr = res.stderr.decode('utf8', 'ignore').strip()
-    return (stdout, stderr, res.returncode)
+    timed_out = False
+    try:
+        res = subprocess.run(
+            command, input=bytearray(sql_statement, 'utf8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=5
+        )
+        stdout = res.stdout.decode('utf8', 'ignore').strip()
+        stderr = res.stderr.decode('utf8', 'ignore').strip()
+        returncode = res.returncode
+    except subprocess.TimeoutExpired:
+        return ("", "", 42, True)
+    return (stdout, stderr, returncode, timed_out)
+
+
+def reproduce_crashes(reproduction_dir, duckdb_cli, file_reader_function):
+    unique_crashes = {}
+    # verify file _REPRODUCTIONS.json exists
+    crashes_json_file = reproduction_dir / 'crashes/_REPRODUCTIONS.json'
+    if not crashes_json_file.is_file():
+        print(f"no crashes found; file not present: {crashes_json_file}")
+        return unique_crashes
+    with open(crashes_json_file, 'r') as repr_file_fd:
+        reproduction_data: list = json.load(repr_file_fd)
+    # verify REPRODUCTION_DIR contains the expected data files
+    for repro_item in reproduction_data:
+        repro_file_path = reproduction_dir / 'crashes' / repro_item['file_name']
+        if not repro_file_path.is_file():
+            raise ValueError(f"file not found: {repro_file_path}")
+    # reproduce crashes
+    for repro_item in reproduction_data:
+        repro_file_path = reproduction_dir / 'crashes' / repro_item['file_name']
+        arguments = ", " + repro_item['arguments'] if repro_item['arguments'] else ""
+        exception_msg, stacktrace = reproduce_file_reader_issue(
+            duckdb_cli, repro_file_path, file_reader_function, arguments
+        )
+        if exception_msg and exception_msg not in unique_crashes:
+            unique_crashes[exception_msg] = (repro_file_path, arguments, exception_msg, stacktrace)
+    return unique_crashes
+
+
+def reproduce_hangs(reproduction_dir, duckdb_cli, file_reader_function):
+    unique_hangs = {}
+    # verify file _REPRODUCTIONS.json exists
+    hangs_json_file = reproduction_dir / 'hangs/_REPRODUCTIONS.json'
+    if not hangs_json_file.is_file():
+        print(f"no hangs found; file not present: {hangs_json_file}")
+        return unique_hangs
+    with open(hangs_json_file, 'r') as repr_file_fd:
+        reproduction_data: list = json.load(repr_file_fd)
+    # verify REPRODUCTION_DIR contains the expected data files
+    for repro_item in reproduction_data:
+        repro_file_path = reproduction_dir / 'hangs' / repro_item['file_name']
+        if not repro_file_path.is_file():
+            raise ValueError(f"file not found: {repro_file_path}")
+    # reproduce hangs (return as soon as 1 has been found)
+    for repro_item in reproduction_data:
+        print("looping over hangs ..")
+        repro_file_path = reproduction_dir / 'hangs' / repro_item['file_name']
+        arguments = ", " + repro_item['arguments'] if repro_item['arguments'] else ""
+        exception_msg, stacktrace = reproduce_file_reader_issue(
+            duckdb_cli, repro_file_path, file_reader_function, arguments
+        )
+        if exception_msg:
+            unique_hangs[exception_msg] = (repro_file_path, arguments, exception_msg, stacktrace)
+            return unique_hangs
+    print("hang could not be reproduced")
+    return unique_hangs
 
 
 def main(argv: list[str]):
@@ -93,49 +177,17 @@ def main(argv: list[str]):
     if len(argv) > 4:
         DUCKDB_FUZZER_DIR = Path(argv[4]).expanduser()
 
-    # issue_file_dir = DUCKDB_FUZZER_DIR / f"issue_files/{file_type}"
-
-    # verify file _REPRODUCTIONS.json exists
-    reproductions_json_file = REPRODUCTION_DIR / 'crashes/_REPRODUCTIONS.json'
-    if not reproductions_json_file.is_file():
-        raise ValueError(f"expected file not found: {reproductions_json_file}")
-    with open(reproductions_json_file, 'r') as repr_file_fd:
-        reproduction_data: list = json.load(repr_file_fd)
-
-    # verify REPRODUCTION_DIR contains the expected data files
-    for repro_item in reproduction_data:
-        repro_file_path = REPRODUCTION_DIR / 'crashes' / repro_item['file_name']
-        if not repro_file_path.is_file():
-            raise ValueError(f"file not found: {repro_file_path}")
-
     # verify duckdb_cli can be found
     duckdb_cli = DUCKDB_DIR / "build/debug/duckdb"
     if not duckdb_cli.is_file():
         raise ValueError(f"expected file not found: {duckdb_cli}")
 
-    # reproduce crashes
-    unique_issues = {}
-    for repro_item in reproduction_data:
-        repro_file_path = REPRODUCTION_DIR / 'crashes' / repro_item['file_name']
-        arguments = ", " + repro_item['arguments'] if repro_item['arguments'] else ""
-        sql_statement = f"from {file_reader_function}('{repro_file_path}'{arguments})"
-        (stdout, stderr, returncode) = run_duckdb(duckdb_cli, sql_statement)
+    # reproduce crashes (keep unique crashes)
+    unique_issues = reproduce_crashes(REPRODUCTION_DIR, duckdb_cli, file_reader_function)
 
-        match returncode:
-            case 0:
-                continue
-            case 1 if not is_internal_error(stderr):
-                continue
-            case 1:
-                exception_msg, stacktrace = split_exception_trace(stderr)
-            case _ if returncode < 0:
-                exception_msg, stacktrace = split_exception_trace(stderr)
-                sig_name = signal.Signals(-returncode).name
-                exception_msg = f"{sig_name}: {exception_msg}"
-            case _:
-                raise ValueError(f"undefined return code: {returncode} (expected 0, 1, or negative values)")
-        if exception_msg not in unique_issues:
-            unique_issues[exception_msg] = (repro_file_path, arguments, exception_msg, stacktrace)
+    # reproduce hangs (keep max 1)
+    unique_hangs = reproduce_hangs(REPRODUCTION_DIR, duckdb_cli, file_reader_function)
+    unique_issues.update(unique_hangs)
 
     # only keep new issues
     new_issues = {}
@@ -155,13 +207,19 @@ def main(argv: list[str]):
             title, rel_file_path, repro_file_path, sql_statement_gh, exception_msg, stacktrace = issue
             run_command(f"cp {repro_file_path} {DUCKDB_FUZZER_DIR / rel_file_path}")
         run_command(f"git -C {DUCKDB_FUZZER_DIR} add .")
-        run_command(f"git -C {DUCKDB_FUZZER_DIR} commit -m 'add reproduction file for afl++ fuzz run {os.environ.get('FUZZ_RUN_ID')}'")
-        run_command(f"git -C {DUCKDB_FUZZER_DIR} push || (git -C {DUCKDB_FUZZER_DIR} config pull.rebase true && git -C {DUCKDB_FUZZER_DIR} pull git -C {DUCKDB_FUZZER_DIR} git push)")
+        run_command(
+            f"git -C {DUCKDB_FUZZER_DIR} commit -m 'add reproduction file for afl++ fuzz run {os.environ.get('FUZZ_RUN_ID')}'"
+        )
+        run_command(
+            f"git -C {DUCKDB_FUZZER_DIR} push || (git -C {DUCKDB_FUZZER_DIR} config pull.rebase true && git -C {DUCKDB_FUZZER_DIR} pull git -C {DUCKDB_FUZZER_DIR} git push)"
+        )
 
     # create github issues
     for issue in new_issues.values():
         title, rel_file_path, repro_file_path, sql_statement_gh, exception_msg, stacktrace = issue
-        fuzzer_helper.file_issue(title, sql_statement_gh, exception_msg, stacktrace, os.environ['FUZZ_SCENARIO'], 0, os.environ['DUCKDB_SHA'])
+        fuzzer_helper.file_issue(
+            title, sql_statement_gh, exception_msg, stacktrace, os.environ['FUZZ_SCENARIO'], 0, os.environ['DUCKDB_SHA']
+        )
         print(sql_statement_gh)
 
 
